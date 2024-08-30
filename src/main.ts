d4871c5ea09184b51985f0216c6eac9abf96343c
aleereza mousavi <aleereza96m@gmail.com>
2024-08-30 20:55:29 +0330

    restructured

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from './core/router'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).use(createPinia()).mount('#app')
