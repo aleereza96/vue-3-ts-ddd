4c528ac526e67450a5e970e95d60a3467ef276e6
aleereza mousavi <aleereza96m@gmail.com>
2024-08-30 23:11:44 +0330

    default layout

--- src/core/layouts/components/DefaultHeader.vue
+++ src/core/layouts/components/DefaultHeader.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="bg-black">Header</div>
+  <div class="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800">
+    <div
+      class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8"
+    >
+      <div class="flex flex-row items-center justify-between p-4">
+        <a
+          href="#"
+          class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
+          >Navbar</a
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped></style>
+<script lang="ts" setup></script>
